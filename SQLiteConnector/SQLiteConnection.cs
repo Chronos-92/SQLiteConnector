93a49fd3bb47cdd9b9398765da446cdefc2daa4c
Glyph <silvester@vanharberden.eu>
2017-02-05 17:12:10 +0100

    Initial Commit.

---
+++ SQLiteConnector/SQLiteConnection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+
+namespace SQLiteConnector
+{
+    public class SQLiteConnection : IDisposable {
+
+        private System.Data.SQLite.SQLiteConnection conn;
+
+        public SQLiteConnection(String Database) {
+            conn = new System.Data.SQLite.SQLiteConnection(string.Format("Data Source={0};Version=3;", Database));
+        }
+        public void Dispose() {
+            conn.Dispose();
+        }
+
+        public void Open() {
+            conn.Open();
+        }
+        public void Close() {
+            conn.Close();
+        }
+
+        public T[] ExecuteQuery<T>(String query) {
+            return conn.CreateCommand(query).ExecuteQuery<T>();
+        }
+        public T[] ExecuteQuery<T>(String query, Dictionary<String, Object> parameters) {
+            return conn.CreateCommand(query, parameters).ExecuteQuery<T>();
+        }
+        public Int64 ExecuteNonQuery(String query) {
+            return conn.CreateCommand(query).ExecuteNonQuery();
+        }
+        public Int64 ExecuteNonQuery(String query, Dictionary<String, Object> parameters) {
+            return conn.CreateCommand(query, parameters).ExecuteNonQuery();
+        }
+        public SQLiteTransaction BeginTransaction() {
+            return conn.BeginTransaction();
+        }
+        public SQLiteTransaction BeginTransaction(IsolationLevel isolationlevel) {
+            return conn.BeginTransaction(isolationlevel);
+        }
+    }
+}
---
+++ SQLiteConnector/SQLiteExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace SQLiteConnector {
+    internal static class SQLiteExtensions {
+        internal static SQLiteCommand CreateCommand(this System.Data.SQLite.SQLiteConnection connection, String command) {
+            return new SQLiteCommand(command, connection);
+        }
+        internal static SQLiteCommand CreateCommand(this System.Data.SQLite.SQLiteConnection connection, String command, Dictionary<String, Object> parameters) {
+            var cmd = connection.CreateCommand(command);
+            cmd.Parameters.AddRange((from p in parameters select new SQLiteParameter(p.Key, p.Value)).ToArray());
+            return cmd;
+        }
+        internal static T[] ExecuteQuery<T>(this SQLiteCommand command) {
+            var result = new List<T>();
+            var reader = command.ExecuteReader();
+            while (reader.Read()) {
+                var instance = (T)Activator.CreateInstance(typeof(T));
+                for (int i = 0; i < reader.FieldCount; i++) {
+                    var prop = typeof(T).GetProperty(reader.GetName(i));
+                    prop.SetValue(instance, reader[i] == DBNull.Value ? null : reader[i], null);
+                }
+                result.Add(instance);
+            }
+            reader.Close();
+            return result.ToArray();
+        }
+    }
+}
